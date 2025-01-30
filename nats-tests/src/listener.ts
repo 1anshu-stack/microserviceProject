2a93c7ab794aa66c157044b55eefa8d5a32bea8a
1anshu-stack <anshuu424@gmail.com>
2025-01-31 00:46:50 +0530

    queue group

--- nats-tests/src/listener.ts
+++ nats-tests/src/listener.ts
@@ -12,9 +12,33 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
     console.log('Listener connected to NATS')
 
-    const subscription = stan.subscribe('ticket:created')
+
+    stan.on('close', () => {
+        console.log('NATS connection closed');
+        process.exit()
+    })
+
+
+    const options = stan.subscriptionOptions().setManualAckMode(true);
+    const subscription = stan.subscribe(
+        'ticket:created', 
+        'orders-service-queue-group',
+        options
+    )
 
     subscription.on('message', (msg)=>{
-        console.log('Message received')
+        const data = msg.getData();
+        if(typeof data === 'string'){
+            console.log(
+                `Received event #${msg.getSequence()}, with data: ${data}`
+            )
+        }
+
+        msg.ack();
     }) 
 })
+
+
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
