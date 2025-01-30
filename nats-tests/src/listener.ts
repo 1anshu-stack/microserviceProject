a1a6fdfb9044817ce8f6c45be1ece8361ad64c3e
1anshu-stack <anshuu424@gmail.com>
2025-01-30 21:37:33 +0530

    listener created

---
+++ nats-tests/src/listener.ts
@@ -0,0 +1,20 @@
+import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+    url: 'http://localhost:4222'
+});
+
+
+stan.on('connect', () => {
+    console.log('Listener connected to NATS')
+
+    const subscription = stan.subscribe('ticket:created')
+
+    subscription.on('message', (msg)=>{
+        console.log('Message received')
+    }) 
+})
