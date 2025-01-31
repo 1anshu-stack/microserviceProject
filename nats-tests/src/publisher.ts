faa36b2b9230672c1b3ad545525412df6264dad4
1anshu-stack <anshuu424@gmail.com>
2025-01-31 12:06:56 +0530

    publisher base listener

--- nats-tests/src/publisher.ts
+++ nats-tests/src/publisher.ts
@@ -1,4 +1,6 @@
 import nats from 'node-nats-streaming';
+import {TicketCreatedPublisher} from './events/ticket-created-publisher';
+
 
 console.clear();
 
@@ -11,14 +13,15 @@ stan.on('connect',async () =>{
     console.log('Publisher connected to NATS');
 
 
-    const data = JSON.stringify({
-        id: '123',
-        title: 'concert',
-        price: 20
-    });
+    const publisher = new TicketCreatedPublisher(stan);
+    try{
+        await publisher.publish({
+            id: '123',
+            title: 'concert',
+            price: 20
+        });
+    } catch(err){
+        console.error(err)
+    }
 
-
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published');
-    })
 })
