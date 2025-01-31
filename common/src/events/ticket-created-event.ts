f7718c5e35702e65e548d3085c0ef3dd6e67c6fc
1anshu-stack <anshuu424@gmail.com>
2025-01-31 13:16:14 +0530

    update

---
+++ common/src/events/ticket-created-event.ts
@@ -0,0 +1,15 @@
+import { Subjects } from "./subjects";
+
+
+export interface TicketCreatedEvent {
+    subject: Subjects.TicketCreated;
+    data: {
+        id: string;
+        title: string;
+        price: number;
+        userId: string;
+    };
+}
+
+
+
