832afbacf2cf26376d9ef5b8821ea5b2549c5e14
1anshu-stack <anshuu424@gmail.com>
2025-02-02 14:53:42 +0530

    update

---
+++ common/src/events/order-created-event.ts
@@ -0,0 +1,17 @@
+import { Subjects } from "./subjects";
+import { OrderStatus } from "@uchihatickets/common";
+
+
+export interface OrderCreatedEvent {
+    subject: Subjects.OrderCreated;
+    data: {
+        id: string;
+        status: OrderStatus;
+        userId: string;
+        expiresAt: string;
+        ticket: {
+            id: string;
+            price: number;
+        };
+    };
+};
